30c97d0dfe3f37bb848e66fef8c911cebccfbae1
Tabaré Caorsi <tabare@heapstersoft.com>
2014-10-14 15:09:26 -0200

    fixed assetic update bug fixed missing menu dep in kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new p1r0\AdminUserBundle\p1r0AdminUserBundle(),
             new p1r0\Tpls\AdminLTEBundle\p1r0TplsAdminLTEBundle(),
         );
